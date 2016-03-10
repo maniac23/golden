9d3166ff5bbbff6642fe514d1563f6c877ac2120
Mikhail Tukai <tukaym@gmail.com>
2016-03-10 20:12:15 +0300

    initial commit

---
+++ js/common.js
@@ -0,0 +1,64 @@
+$(document).ready(function () {
+
+    $(".slideinleft").animated("slideInLeft", "slideOutLeft");
+    $(".slideinright").animated("slideInRight", "slideOutRight");
+    $(".fadein").animated("fadeInDown", "fadeOutDown");
+
+    $(".nav a, .top").mPageScroll2id({
+        scrollSpeed: 700
+    });
+
+
+    //Цели для ндекс.Метрики и Google Analytics
+    $(".count_element").on("click", (function () {
+        ga("send", "event", "goal", "goal");
+        yaCounterXXXXXXXX.reachGoal("goal");
+        return true;
+    }));
+
+    //SVG Fallback
+    if (!Modernizr.svg) {
+        $("img[src*='svg']").attr("src", function () {
+            return $(this).attr("src").replace(".svg", ".png");
+        });
+    };
+
+    //Аякс отправка форм
+    //Документация: http://api.jquery.com/jquery.ajax/
+    $("#form").submit(function () {
+        $.ajax({
+            type: "POST",
+            url: "mail.php",
+            data: $(this).serialize()
+        }).done(function () {
+            alert("Спасибо за заявку!");
+            setTimeout(function () {
+
+                $("#form").trigger("reset");
+            }, 1000);
+        });
+        return false;
+    });
+
+    //Chrome Smooth Scroll
+    try {
+        $.browserSelector();
+        if ($("html").hasClass("chrome")) {
+            $.smoothScroll();
+        }
+    } catch (err) {
+
+    };
+
+    $("img, a").on("dragstart", function (event) {
+        event.preventDefault();
+    });
+
+});
+
+$(window).load(function () {
+
+    $(".loader_inner").fadeOut();
+    $(".loader").delay(400).fadeOut("slow");
+
+});
